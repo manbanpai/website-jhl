ddc077fa7998b46ec382f4a8b3b586ef23fe01f0
manbanpai <761846158@qq.com>
2017-10-11 16:10:43 +0800

    it

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -6,7 +6,7 @@
 <meta name="viewport"
 	content="width=device-width, initial-scale=1, user-scalable=yes">
 
-<title>HANKS hydraulic hoses Italy</title>
+<title>HANKS hydraulic hoses</title>
 <link rel="stylesheet" href="/css/css.css" media="all">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta name="description"
